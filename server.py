c0e32532f6b6bf28923a219492d540949ea8bcf7
Adjuzi <adaan690@student.liu.se>
2019-02-04 11:00:36 +0100

    started on server.py

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/sign_in', methods=['POST'])
+def s_in():
+    if valid_login(request.form['email'],
+                    request.form['password']):
+        # Skapa token till användare
+        return log_the_user_in(request.form['username'])
+    else:
+        error = 'Invalid username/password'
