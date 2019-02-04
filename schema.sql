40fcdb731f56ef5f3cfee2d9fb48acec612d8bc1
Anton Dalgren <anton@84codes.com>
2019-02-04 10:12:15 +0100

    Added schema.sql and updated .gitignore

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+ id INTEGER PRIMARY KEY,
+ first_name TEXT NOT NULL,
+ family_name TEXT NOT NULL,
+ email text NOT NULL UNIQUE,
+ city text NOT NULL,
+ country text NOT NULL,
+ gender text NOT NULL,
+ password text NOT NULL
+);
+
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+ id INTEGER PRIMARY KEY,
+ message TEXT NOT NULL,
+ to_id INTEGER NOT NULL,
+ from_ id INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS active_users;
+CREATE TABLE active_users (
+ id INTEGER PRIMARY KEY,
+ token TEXT NOT NULL
+);
+
+
